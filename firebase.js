b3e1b1c4ea6baa1a53b551914f773e9e60e0b04b
paulin236 <132255748+paulin236@users.noreply.github.com>
2024-09-05 15:25:32 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,3 +1,4 @@
+import { getAuth } from "firebase/auth";
   var config = {
     apiKey: "AIzaSyAj7cdG9iGRlyrCdz04QaKtp0fXberB41g",
   authDomain: "holamundo-b14a9.firebaseapp.com",
@@ -8,3 +9,5 @@
   appId: "1:265875831725:web:d40b1630065551b9787f24"
   };
   firebase.initializeApp(config);
+  // Initialize Firebase Authentication and get a reference to the service
+  const auth = getAuth(app);
